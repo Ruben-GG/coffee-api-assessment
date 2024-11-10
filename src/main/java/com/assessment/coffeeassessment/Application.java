1b914db85b3928abf21f9c04ed7a5760658c6629
Ruben Garcia Gallardo <gallardogruben@gmail.com>
2024-11-10 19:39:39 +0100

    FEAT: add project folder structure for models, services, controllers and resources

--- src/main/java/com/assessment/coffeeassessment/Application.java
+++ src/main/java/com/assessment/coffeeassessment/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoffeeAssessmentApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoffeeAssessmentApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
