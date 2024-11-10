ce7ccecb30667a5a437ca7416b252830657cc402
Ruben Garcia Gallardo <gallardogruben@gmail.com>
2024-11-10 19:27:48 +0100

    INIT: initial commit with basic Spring Boot setup

---
+++ src/main/java/com/assessment/coffeeassessment/CoffeeAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.coffeeassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeAssessmentApplication.class, args);
+	}
+
+}
